4d8c20c8069efd390cef0a1471d42ee5d1c86cf6
caio vinicius malvezzi <caiomalvezzi101@gmail.com>
2022-09-21 14:32:55 -0300

    Componentes e autenticação com supabase

---
+++ src/lib/supabase.ts
@@ -0,0 +1,16 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js'
+
+const PASSWORD = 'h9tPBFj@8kYfyV7'
+
+const supabaseUrl = 'https://nkmzhitbypysjmtmfkwf.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rbXpoaXRieXB5c2ptdG1ma3dmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2Mzc4MDAzNCwiZXhwIjoxOTc5MzU2MDM0fQ.Rmcw0lUr8ufVu__E8z7wg5tm3gaQFX72P89aijhbf3k'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
