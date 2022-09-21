bd8af993d59549ef923c21ccc763bdeedd2ed9e9
caio vinicius malvezzi <caiomalvezzi101@gmail.com>
2022-09-21 15:50:44 -0300

    Corrigindo identação e alteração de template de email

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js'
 
-const PASSWORD = 'h9tPBFj@8kYfyV7'
-
 const supabaseUrl = 'https://nkmzhitbypysjmtmfkwf.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rbXpoaXRieXB5c2ptdG1ma3dmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2Mzc4MDAzNCwiZXhwIjoxOTc5MzU2MDM0fQ.Rmcw0lUr8ufVu__E8z7wg5tm3gaQFX72P89aijhbf3k'
 
